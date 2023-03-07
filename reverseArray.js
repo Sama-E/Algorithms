cfb75e6cea22370c673868375cc5550b237464de
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-03-07 14:48:11 -0700

    Create reverseArray.js

---
+++ reverseArray.js
@@ -0,0 +1,15 @@
+function reverse(arr){
+    var rev = [];
+    //Empty array
+    for(let i=arr.length-1; i >= 0; i--){
+    //i = array index
+    //let i = length of array -1, start at the end of the array
+    //when i is greater than 0
+    //next i = i-1
+        rev.push(arr[i]);
+        //push element(arr[i]) to new array
+    }
+    console.log(rev);
+}
+
+reverse(["a", "b", "c", "d", "e"]);

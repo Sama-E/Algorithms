2a080e5cd8e592dcae1218fe95493b34bea7aa7e
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-05-09 19:53:21 -0600

    Update invertObject.js

--- invertObject.js
+++ invertObject.js
@@ -1,8 +1,8 @@
 /* /* /* 
-  Invert Hash
-  A hash table / hash map is an obj / dictionary
-  Given an object / dict,
-  return a new object / dict that has the keys and the values swapped so that the keys become the values and the values become the keys
+    Invert Hash
+    A hash table / hash map is an obj / dictionary
+    Given an object / dict,
+    return a new object / dict that has the keys and the values swapped so that the keys become the values and the values become the keys
 */
 
 const obj1 = { name: "Zaphod", charm: "high", morals: "dicey" };
